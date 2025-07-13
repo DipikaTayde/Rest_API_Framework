9e82d2120b4346093ad008d2edc34297ffff2bad
DipikaTayde <155555426+DipikaTayde@users.noreply.github.com>
2025-07-13 23:14:55 +0530

    Delete src/test/java directory

--- src/test/java/MainMenthod.java
+++
@@ -1,8 +0,0 @@
-import javax.xml.soap.SOAPPart;
-
-public class MainMenthod {
-    public static void main(String [] args){
-        System.out.println(args.length);
-
-    }
-}
--- src/test/java/Test10.java
+++
@@ -1,7 +0,0 @@
-public class Test10 {
-    public static void main(String[] args) {
-        // This is a test is a program is
-
-
-    }
-}
